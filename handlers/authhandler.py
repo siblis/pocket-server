118a576756aacdd7cf766db5b832c97854d50ac6
kaktycc <kaktycc@rambler.ru>
2018-08-26 13:25:02 +0300

    Исправлен импорт модуля, post поменян на put

--- handlers/authhandler.py
+++ handlers/authhandler.py
@@ -1,7 +1,8 @@
-from handlers.jsonhandler import JsonHandler
+from handlers.json_util import JsonHandler
 import hashlib
 from salt import salt
 from database_tools.alchemy import CUsers
+import secrets
 
 
 class AuthHandler(JsonHandler):
@@ -11,28 +12,29 @@ class AuthHandler(JsonHandler):
     def create_sha(self, string):
         return hashlib.sha256(string.encode() + salt.encode()).hexdigest()
 
-    def post(self):
+    def put(self):
         login = self.json_data['user']
         passwd = self.json_data['password']
         passwd = self.create_sha(passwd)
         exists = self.db.query(CUsers).filter_by(username=login).all()
         if exists:
-            result_sha = self.db.query(CUsers.password).filter_by(username=login).first()[0]
+            result_db = self.db.query(CUsers.password).filter_by(username=login).first()[0]
+            result_sha = self.create_sha(result_db)
             if passwd == result_sha:
                 self.write('You logged success\n')
                 token = secrets.token_hex(32)
                 #  запись token
-                self.db.query(CUsers).filter(CUsers.login == login).update({CUsers.token: token},
+                self.db.query(CUsers).filter(CUsers.username == login).update({CUsers.token: token},
                                                                            synchronize_session='evaluate')
                 self.response['token'] = token
                 self.response['response'] = '200'
                 self.write_json()
             else:
+                self.response['message'] = 'Incorrect password'
                 self.response['response'] = '403'
                 self.write_json()
-                self.write('Incorrect password')
 
         else:
-            self.write('Login or password incorrect\n')
+            self.response['message'] = 'Login or password incorrect'
             self.response['response'] = '403'
             self.write_json()
