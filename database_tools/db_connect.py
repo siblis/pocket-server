6d24b41be6f0538c085084733fd1da5f41218fd2
kaktycc <kaktycc@rambler.ru>
2018-09-03 19:11:21 +0300

    Добавлена функция подключения к БД с указанием имени БД

--- database_tools/db_connect.py
+++ database_tools/db_connect.py
@@ -15,3 +15,16 @@ engine = create_engine(db_address)
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
+
+def connect(dbname):
+    POSTGRES_SERVER = 'localhost'
+    POSTGRES_PORT = '5432'
+    POSTGRES_LOGIN = 'postgres'
+    POSTGRES_PASS = '123'
+    POSTGRES_BASE = dbname
+
+    db_address = 'postgresql+psycopg2://{0}:{1}@{2}:{3}/{4}'.format(POSTGRES_LOGIN, POSTGRES_PASS, POSTGRES_SERVER,
+                                                                    POSTGRES_PORT,
+                                                                    POSTGRES_BASE)
+    engine = create_engine(db_address)
+    return engine
