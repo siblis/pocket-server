7365debf429ca3bded61445c6b1f17b487a49b3c
kaktycc <kaktycc@rambler.ru>
2018-09-04 18:41:13 +0300

    Добавлен класс для работы с ws, отправка сообщения всем подключенным пользователям

--- handlers/wshandler.py
+++ handlers/wshandler.py
@@ -1,23 +1,46 @@
 import tornado.websocket
+import uuid
+from handlers.json_util import JsonHandler
+from database_tools.db_connect import Session
 
-# open websocket and retrieve msg
-class WebSocketHandler(tornado.websocket.WebSocketHandler):
+session = Session()
+
+
+class WebSocketHandler(tornado.websocket.WebSocketHandler, JsonHandler):
+    ws_clients = []
+
+    @staticmethod
+    def _gen_session():
+        return uuid.uuid4()
+
+    def prepare(self):
+        if 'Token' in self.request.headers:
+            self._token_check(session)
+        else:
+            self.close(401)
+
+    def check_origin(self, origin):
+        return True
+
     def open(self):
         print("WebSocket opened")
+        self.session = self._gen_session()
         self.application.webSocketsPool.append(self)
+        self.ws_clients.append(self.session)
+        print(self.ws_clients)
 
     def on_message(self, message):
         self.write_message(u"You said: " + message)
-        print('Come message ' + message)
-        # db = self.application.db
-        # message_dict = json.loads(message)
-        # db.chat.insert(message_dict)
+        print(f'Come message {message} from {self.session}')
+
         for key, value in enumerate(self.application.webSocketsPool):
             if value != self:
                 value.ws_connection.write_message(message)
 
     def on_close(self):
         print("WebSocket closed")
+        self.ws_clients.remove(self.session)
         for key, value in enumerate(self.application.webSocketsPool):
             if value == self:
                 del self.application.webSocketsPool[key]
+        print(self.ws_clients)
