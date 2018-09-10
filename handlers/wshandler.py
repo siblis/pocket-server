c73d48e12eb40c7655d5c138555518c306945e77
Evgeniy Kaktycc <kaktycc777@gmail.com>
2018-09-10 18:20:27 +0300

    Исправил  token_check, список исправлен на кортеж

--- handlers/wshandler.py
+++ handlers/wshandler.py
@@ -1,43 +1,46 @@
 import tornado.websocket
 import uuid
 from handlers.json_util import JsonHandler
-from database_tools.db_connect import Session
+from typing import NamedTuple
+
+
+class UserData(NamedTuple):
+    user_id: int
+    ws_object: object
 
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler, JsonHandler):
     ws_dict = dict()
 
-    @staticmethod
-    def _gen_session():
+    def _gen_session(self):
         return uuid.uuid4()
 
     def prepare(self):
         if 'Token' in self.request.headers:
-            self.uid = self._token_check(self.db)
+            self.uid = self._token_check()
         else:
             self.close(401)
 
     def open(self):
-        temp_list = []
         print("WebSocket opened")
         # ген сессии
         self.session = self._gen_session()
-        temp_list.append(self.uid)
-        temp_list.append(self)
+        self.user_tuple = UserData(self.uid, self)
         # обьект ws
         self.application.webSocketsPool.append(self)
         # словарь ключ сессия, объект ws
-        self.ws_dict[self.session] = temp_list
+        self.ws_dict[self.session] = self.user_tuple
 
     def on_message(self, message):
-        print(self.ws_dict)
         self.write_message(u"You said: " + message)
+        # print(f'Come message {message} from {self.session}')
         print(f'Come message {message} from id {self.uid} and sessid {self.session}')
-
         for key in self.ws_dict.keys():
             if key != self.session:
-                self.ws_dict[key][1].write_message(message)
+                self.ws_dict[key].ws_object.write_message(message)
 
     def on_close(self):
         print("WebSocket closed")
         self.ws_dict.pop(self.session)
+
+
