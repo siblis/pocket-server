177b2f828ddc73a04552ba4bb92f81d8b61f67f5
kaktycc <kaktycc@rambler.ru>
2018-09-14 23:02:09 +0300

    Добавлена возможность отправлять одному пользователю, добавлены коды ответов

--- handlers/wshandler.py
+++ handlers/wshandler.py
@@ -2,6 +2,7 @@ import tornado.websocket
 import uuid
 from handlers.json_util import JsonHandler
 from typing import NamedTuple
+import tornado.escape
 
 
 class UserData(NamedTuple):
@@ -11,6 +12,9 @@ class UserData(NamedTuple):
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler, JsonHandler):
     ws_dict = dict()
+
+    def check_origin(self, origin):
+        return True
 
     def _gen_session(self):
         return uuid.uuid4()
@@ -22,25 +26,38 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler, JsonHandler):
             self.close(401)
 
     def open(self):
-        print("WebSocket opened")
-        # ген сессии
+        # TODO в лог
+        # print("WebSocket opened")
         self.session = self._gen_session()
         self.user_tuple = UserData(self.uid, self)
-        # обьект ws
-        self.application.webSocketsPool.append(self)
-        # словарь ключ сессия, объект ws
+
         self.ws_dict[self.session] = self.user_tuple
 
     def on_message(self, message):
-        self.write_message(u"You said: " + message)
-        # print(f'Come message {message} from {self.session}')
-        print(f'Come message {message} from id {self.uid} and sessid {self.session}')
-        for key in self.ws_dict.keys():
-            if key != self.session:
-                self.ws_dict[key].ws_object.write_message(message)
+        #TODO в лог и этот принт
+        # print(f'Come message {message} from id {self.uid} and sessid {self.session}')
+        json_data = ''
+        try:
+            json_data = tornado.escape.json_decode(message)
+            json_data['sender'] = self.session
+            for key in self.ws_dict.keys():
+                if key != self.session:
+                    if self.ws_dict[key].user_id == int(json_data['receiver']):
+                        self.ws_dict[key].ws_object.write_message(json_data['message'])
+                        self.write_message({"response": "200"})
+                    else:
+                        self.write_message({"response": "404", "message": "Client not found or not online"})
+                else:
+                    if len(self.ws_dict) == 1:
+                        self.write_message({"response": "404", "message": "Not found receiver"})
+        except ValueError:
+            message = 'Unable to parse JSON'
+            self.write_message({"response": "400", "message": message})
+        except Exception as e:
+            message = 'Bad JSON'
+            self.write_message({"response": "400", "message": message})
+
 
     def on_close(self):
-        print("WebSocket closed")
+        # print("WebSocket closed")
         self.ws_dict.pop(self.session)
-
-
