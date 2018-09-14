0813af37f1f62c36a1913c99d5e146fc6b353be0
kaktycc <kaktycc@rambler.ru>
2018-09-14 22:42:56 +0300

    Исправлено подключение к БД, настройки в отдельном файле

--- database_tools/db_connect.py
+++ database_tools/db_connect.py
@@ -1,12 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-POSTGRES_SERVER = 'localhost'
-POSTGRES_PORT = '5432'
-POSTGRES_LOGIN = 'postgres'
-POSTGRES_PASS = '123'
-POSTGRES_BASE = 'messengerbase'
+from database_tools.db_config import POSTGRES_LOGIN, POSTGRES_PASS, POSTGRES_SERVER, POSTGRES_PORT, POSTGRES_BASE
 
 db_address = 'postgresql+psycopg2://{0}:{1}@{2}:{3}/{4}'.format(POSTGRES_LOGIN, POSTGRES_PASS, POSTGRES_SERVER,
                                                                 POSTGRES_PORT,
@@ -16,13 +11,8 @@ Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
 
-def connect(dbname):
-    POSTGRES_SERVER = 'localhost'
-    POSTGRES_PORT = '5432'
-    POSTGRES_LOGIN = 'postgres'
-    POSTGRES_PASS = '123'
-    POSTGRES_BASE = dbname
 
+def connect(dbname):
     db_address = 'postgresql+psycopg2://{0}:{1}@{2}:{3}/{4}'.format(POSTGRES_LOGIN, POSTGRES_PASS, POSTGRES_SERVER,
                                                                     POSTGRES_PORT,
                                                                     POSTGRES_BASE)
