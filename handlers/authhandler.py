a2d55b8ab9da11f3ce35ccdfc39c54f7642f6b96
kaktycc <kaktycc@rambler.ru>
2018-09-16 18:25:00 +0300

    Шифрование пароля в отдельном классе, исправлено в остальных

--- handlers/authhandler.py
+++ handlers/authhandler.py
@@ -1,6 +1,4 @@
 from handlers.json_util import JsonHandler
-import hashlib
-from salt import salt
 from database_tools.alchemy import CUsers
 import secrets
 
@@ -9,17 +7,14 @@ class AuthHandler(JsonHandler):
     def get(self):
         self.write('GET - Welcome to the AuthHandler!')
 
-    def create_sha(self, string):
-        return hashlib.sha256(string.encode() + salt.encode()).hexdigest()
-
     def put(self):
         login = self.json_data['user']
         passwd = self.json_data['password']
-        passwd = self.create_sha(passwd)
+        passwd = self._create_sha(passwd)
         exists = self.db.query(CUsers).filter_by(username=login).all()
         if exists:
             result_db = self.db.query(CUsers.password).filter_by(username=login).first()[0]
-            result_sha = self.create_sha(result_db)
+            result_sha = self._create_sha(result_db)
             if passwd == result_sha:
                 self.write('You logged success\n')
                 token = secrets.token_hex(32)
