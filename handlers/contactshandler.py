e4731d572dc02909a2987427687cea29d9a89b6d
kaktycc <kaktycc@rambler.ru>
2018-10-09 11:57:47 +0300

    Добавлен шаблон salt, добавлен отдельный хендлер для контактов ч

---
+++ handlers/contactshandler.py
@@ -0,0 +1,51 @@
+from handlers.json_util import JsonHandler
+from database_tools.alchemy import CUsers, CContacts
+
+
+class ContactsHandler(JsonHandler):
+    def prepare(self):
+        JsonHandler.prepare(self)
+        self.check_result = self._token_check()
+
+    def post(self):
+        if self.check_result:
+            contact = self.json_data['contact']
+            exists_contact = self.db.query(CUsers).filter(CUsers.email == contact).one_or_none()
+            if exists_contact is None:
+                self.set_status(404, 'User does not exists')
+            else:
+                result = self.db.query(CContacts).filter(CContacts.user_id == self.check_result.uid,
+                                                         CContacts.contact == exists_contact.uid).first()
+                if result is None:
+                    new_contact = CContacts(user_id=self.check_result.uid, contact=exists_contact.uid)
+                    self.db.add(new_contact)
+                    self.db.commit()
+                    self.set_status(201, 'Added')
+                else:
+                    self.set_status(409, 'Contact already in list')
+
+    def delete(self):
+        if self.check_result:
+            contact = self.json_data['contact']
+            result = self.db.query(CUsers).filter(CUsers.email == contact).one_or_none()
+            result_db = self.db.query(CContacts).filter(CContacts.user_id == self.check_result.uid,
+                                                        CContacts.contact == result.uid).delete()
+            if not result_db:
+                self.set_status(404, 'Not in your contact list')
+            else:
+                self.db.commit()
+                self.set_status(200)
+                self.response['deleted_contact_id'] = result.uid
+                self.response['deleted_contact_username'] = result.username
+                self.write_json()
+        else:
+            self.send_error(400)
+
+    def get(self):
+        if self.check_result:
+            contacts = self.db.query(CContacts, CUsers).filter(CContacts.user_id == self.check_result.uid)
+            query = contacts.join(CUsers, CUsers.uid == CContacts.contact)
+            records = query.all()
+            for i in range(len(records)):
+                self.response[i] = records[i].CUsers.username
+            self.write_json()
