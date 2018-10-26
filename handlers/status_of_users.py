259ab5ee8c9e84eba921b950a60849190dbd643c
def1988 <htcdef@gmailc.om>
2018-10-26 09:37:12 +0300

    сделан обработчик get запросов (по названию статуса получаем его id или ошибка)

--- handlers/status_of_users.py
+++ handlers/status_of_users.py
@@ -2,8 +2,32 @@ from sqlalchemy import update
 from handlers.json_util import JsonHandler
 from database_tools.alchemy import CUserStatus
 
+
+# запросы к БД
+# Запросы к редактированию пользоватлеьких статусов
+def get_status_id_users(session, status):
+    return session.query(CUserStatus).filter_by(status_name=status).first()
+
+
 class StatusOfUsers(JsonHandler):
     """
     Класс для созданию, удалению, изменению статусов которые можно будет присовить пользователям
     """
-    pass
+
+    def get(self, status):
+        # Необходимо ограничить доступ к команде (сделать только для админов сервера)
+        check_result = self._token_check()
+        if check_result:
+            try:
+                result = get_status_id_users(self.db, status)
+            except:
+                self.send_error(500, message='Internal Server Error')
+            else:
+                if result is not None:
+                    self.response['users_status_id'] = result.usid
+                    self.set_status(200)
+                    self.write_json()
+                else:
+                    self.send_error(404, message='Status does not exists')
+        else:
+            self.send_error(400, message='Error token')
