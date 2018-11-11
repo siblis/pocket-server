a4d3e8a467ddefb4ac45f38940d079a254bd9739
Yuriy <monguste@gmail.com>
2018-11-11 09:50:44 +0300

    Правка grouphandler, добавил put для добавления групп в коллекцию

--- handlers/grouphandler.py
+++ handlers/grouphandler.py
@@ -1,5 +1,5 @@
 from handlers.json_util import JsonHandler
-from database_tools.alchemy import CGroups
+from database_tools.alchemy import CGroups, CCategoryGroup, CCollGroup
 
 
 class GroupHandler(JsonHandler):
@@ -28,9 +28,11 @@ class GroupHandler(JsonHandler):
 
             if exists_group is None:
                 group_name = self.json_data['group_name']
-                creation_time = self.json_data['creation_time']
+                creation_date = self.json_data['creation_time']
                 creater_user_id = self.json_data['creater_user_id']
-                group = CGroups(group_name=group_name, creation_time=creation_time, creater_user_id=creater_user_id)
+                category_group = self.db.query(CCategoryGroup).filter(CCategoryGroup.category_name == self.json_data['category_group'])  ########
+                group = CGroups(group_name=group_name, creation_date=creation_date,
+                                creater_user_id=creater_user_id, category_group=category_group.category_id)  ########
                 self.db.add(group)
                 self.db.commit()
                 self.set_status(201, reason='Created')
@@ -38,6 +40,34 @@ class GroupHandler(JsonHandler):
             else:
                 message = 'Group already exists'
                 self.send_error(409, message=message)
+
+####################################
+    def put(self):
+        """ Добавление в "супер-группу" """
+
+        if self.check_result:
+            check_group = None
+            try:
+                check_group = self.db.query(CGroups.group_name).filter(CGroups.group_name == self.json_data['group_name'])
+            except:
+                self.send_error(400, message='Please check group_name')
+            try:
+                check_group_add = self.db.query(CGroups.group_name).filter(CGroups.group_name == self.json_data['add_group'])
+            except:
+                self.send_error(400, message='Please check add_group')
+
+            if check_group is None:
+                message = 'Group no found'
+                self.send_error(409, message=message)
+            else:
+                group = self.db.query(CGroups).filter(CGroups.group_name == self.json_data['group_name'])
+                add_group = self.db.query(CGroups).filter(CGroups.group_name == self.json_data['add_group'])
+                coll_group = CCollGroup(collgroup_id=group.gid, group_id=add_group.gid)
+                self.db.add(coll_group)
+                self.db.commit()
+                self.set_status(201, reason='Add group')
+                self.write_json()
+####################################
 
     def delete(self):
         if self.check_result:
