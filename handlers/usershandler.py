0225af6396dac539570e57aeab21f20faa4a1089
Yuriy <monguste@gmail.com>
2018-11-11 18:48:48 +0300

    Правка userhandler

--- handlers/usershandler.py
+++ handlers/usershandler.py
@@ -70,11 +70,11 @@ class UsersHandler(JsonHandler):
             try:
                 user_uid = self.json_data['uid']
                 user = self.db.query(CUsers).filter(CUsers.uid == user_uid).one_or_none()  # first or default
-                """!!!!!!!!"""
-                status_id = user.status_id  ##############
                 if user is None:
                     self.set_status(404, 'User not found')
                 else:
+                    """!!!!!!!!"""
+                    status_id = user.status_id  ##############
                     user = self.json_data['account_name']
                     password = self.json_data['password']
                     password = self._create_sha(password)
